f5368571d33ac11e2dc388281fb46b1e226361af
spen0007 <spen0007@users.noreply.github.com>
2020-11-22 22:15:53 +0000

    help added

--- mkBMP.py
+++ mkBMP.py
@@ -1,14 +1,15 @@
-import struct
 from csv import reader
 import math
 import sys
 import getopt
+import struct
 
 ################################################################################
-#                                   DNA Sequence to bitmap v1.1
-#           Added scale
+#                                   DNA Sequence to bitmap v2.0
+#           
 ################################################################################
-# Scales a bitmap
+
+
 
 
 #Creates a bitmap header
@@ -59,7 +60,7 @@ def intToByteString(int, size):
 
 #Given a two dimentional array of DNA words (as GTAC), representing colors i.e r for G, create a bytearray
 
-def genBitMap(array):
+def genBitMap(array, guanine, thymine, adenine, cytosine):
     rowLen = len(array[0])  #How many rows this bitmap contains
     padding = 4 - ((rowLen * 3) % 4) #Each row must be a multiple of 4, if not padding must be appended.
     bitmap = bytearray()
@@ -67,7 +68,7 @@ def genBitMap(array):
     for i in reversed(range(len(array))): #Each Row 
         for j in range(len(array[i])): #Each Col
                 
-                bitmap += charToRGB(array[i][j])
+                bitmap += charToRGB(array[i][j], guanine, thymine, adenine, cytosine)
           
         if(padding != 4):
             for k in range(padding):
@@ -76,7 +77,7 @@ def genBitMap(array):
 
     return bitmap
 
-def genBitMap2(array, rowLen, rows, scale):
+def genBitMap2(array, rowLen, rows, scale, guanine, thymine, adenine, cytosine):
     
    
     useThisManyBytes=int(rows*rowLen)  #fixes nasty half row endings.
@@ -87,7 +88,7 @@ def genBitMap2(array, rowLen, rows, scale):
     for i in reversed(range(useThisManyBytes)): #read it backwards!
                 pixelCounter += 1
                 for l in range(scale):
-                    lineBitmap += charToRGB(array[i]) #add the pixel scale times
+                    lineBitmap += charToRGB(array[i], guanine, thymine, adenine, cytosine) #add the pixel scale times
                 if pixelCounter == rowLen:
                     pixelCounter =0
                     for m in range(scale):
@@ -100,19 +101,27 @@ def genBitMap2(array, rowLen, rows, scale):
 #Convert a character/string into an rgb byte array.
 #This can read DNA letters (GTAC) and give each a colour
 #Colour is 24 bit encoded RGB
-def charToRGB(char):
-
+def charToRGB(char, guanine, thymine, adenine, cytosine):
+    # GTAC is passed in as string representing hex colour code
     
+    
+
     if(char.lower() == "g"):
-        return encodeRGB(255,0,0) #G is Red
+        return encodeRGB(guanine[0],guanine[1],guanine[2]) #G is Red
     if(char.lower() == "t"):
-        return encodeRGB(0,255,0) #T is Green
+        return encodeRGB(thymine[0],thymine[1],thymine[2]) #T is Green
     if(char.lower() == "a"):
-        return encodeRGB(0,0,255) #A is Blue
+        return encodeRGB(adenine[0],adenine[1],adenine[2]) #A is Blue
     if(char.lower() == "c"):
-        return encodeRGB(0,0,0) #C is Black
+        return encodeRGB(cytosine[0],cytosine[1],cytosine[2]) #C is Black
 
     return encodeRGB(255,255,255) #Default is white if not a DNA word
+
+
+def hex2rgb(h):
+    return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+
 
 ################################################################################
 #                               Start
@@ -122,31 +131,36 @@ def charToRGB(char):
  
 def main(argv):
 
-
-   
+    #set up default colours
+    cytosine = "ff0000"# [255,0,0] # red
+    guanine = "00ff00"  #[0,255,0] green
+    adenine = "0000ff"# [0,0,255] # blue
+    thymine = "000000" #[0,0,0] # black  
     inputfile = ''
     outputfile = ''
+    strScale = "1"
     try:
-        opts, args = getopt.getopt(argv,"hi:o:s:",["inputfile=","outputfile=","help","scale"])
+        opts, args = getopt.getopt(argv,"hi:o:s:c:a:t:g:",["inputfile=","outputfile=","help","scale","cytosine","adenine","thymine","guanine"])
     except getopt.GetoptError:
-        print "mkBMP.py -i <inputfile> -o <outputfile> -s <scalefactor>"
-        print "mkBMP.py --inputfile <inputfile> --outputfile <outputfile> --scalefactor <scalefactor>"
+        print ("mkBMP.py -i <inputfile> -o <outputfile> -s <scalefactor>")
+        print ("mkBMP.py --inputfile <inputfile> --outputfile <outputfile> --scalefactor <scalefactor>")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print "mkBMP.py -i <inputfile> -o <outputfile> -s <scalefactor>"
-            print "mkBMP.py --inputfile <inputfile> --outputfile <outputfile> --scalefactor <scalefactor>"
-            print ""
-            print "input file is a txt file with genetic data"
-            print "input file must only contain GTAC with no control codes such as line feed or carriage return"
-            print ""
-            print "output file is the destination of the bitmap"
-            print "NB if the file exists it will be clobbered"
-            print ""
-            print "scale factor is a factor to enlarge each pixel by"
-            print "scale factor must be an unsigned integer ie >1 and a whole number"
-            print "we are not shrinking the bitmap and we are not having half pixels"
-            print ""
+            print ("mkBMP.py -i <inputfile> -o <outputfile> -s <scalefactor>")
+            print ("mkBMP.py --inputfile <inputfile> --outputfile <outputfile> --scalefactor <scalefactor>\n")
+            print ("input file is a txt file with genetic data in plain text GTAC etc\n")
+            print ("output file is the destination of the bitmap")
+            print ("NB if the file exists it will be clobbered\n")
+            print ("scale factor is a factor to enlarge each pixel by")
+            print ("scale factor must be a positive whole number\n")
+            print ("default colour scheme can be overriden")
+            print ("the switches are -g -t -a -c for guanine, thymine, adenine, and cytosine respectively")
+            print ("the colour should be specified as a 24bit hex colour\n")
+            print ("go to https://htmlcolorcodes.com for a colour picker which provides hexcodes\n\n")
+            print ("NB limit input checking or error checking, be careful\n\n")
+            print ("example:   python mkBMP.py -i dna.txt -o myDNA.bmp -s 16 -c 8B4232 -a 155995 -g DEA246 -t 462945")
+          
             sys.exit()
         elif opt in ("-i", "--inputfile"):
             inputfile = arg
@@ -154,30 +168,38 @@ def main(argv):
             outputfile = arg
         elif opt in ("-s", "--scalefactor"):
             strScale = arg
+        elif opt in ("-c", "--cytosine"):
+            cytosine = arg.lstrip('#')
+        elif opt in ("-a", "--adenine"):
+            adenine = arg.lstrip('#')
+        elif opt in ("-t", "--thymine"):
+            thymine = arg.lstrip('#')
+        elif opt in ("-g", "--gaunine"):
+            guanine = arg.lstrip('#')    
 
     if len(inputfile)==0:
-        print "No input file given"
-        print "mkBMP.py -h for help"
+        print ("No input file given")
+        print ("mkBMP.py -h for help")
         sys.exit(2)
     if len(outputfile)==0:
-        print "No output file given"
-        print "mkBMP.py -h for help"
+        print ("No output file given")
+        print ("mkBMP.py -h for help")
         sys.exit(2)    
-    print "Reading genetic data from: ", inputfile
-    print "Writing bitmap to: ", outputfile  
+    print ("Reading genetic data from: ", inputfile)
+    print ("Writing bitmap to: ", outputfile)
     try :
         scale = int(strScale)
     except:
-        print "Cannot cast " + strScale +  " to int"
-        print "scalefactor must be >1 and a whole number"
+        print ("Cannot cast " + strScale +  " to int")
+        print ("scalefactor must be >1 and a whole number")
         exit (2)
     if scale <1:
         scale = 1
-        print "Warning: scale was set to ", scale
-        print "Shrinking is not implemented"
-        print "Resetting to scalefactor = 1"
-        print "" 
-    print "Scale factor: ", scale 
+        print ("Warning: scale was set to ", scale)
+        print ("Shrinking is not implemented")
+        print ("Resetting to scalefactor = 1")
+        print ("") 
+    print ("Scale factor: ", scale) 
     
 
 
@@ -187,11 +209,20 @@ def main(argv):
     with open (inputfile, 'r') as readObject:
         rawArray=readObject.read()
 
-        stripped = lambda s: "".join(i for i in s if 31 < ord(i) < 127)
-        imageArray = stripped(rawArray)
+    stripped = lambda s: "".join(i for i in s if 31 < ord(i) < 127)
+    imageArray = stripped(rawArray)
     rowLen = 256  #How many rows this bitmap contains before scaling
     rows = math.floor(len(imageArray)/rowLen)  # BMP can't have incomplete rows, no padding so chop the incomplete row
-    pixelArray = genBitMap2(imageArray, rowLen, rows, scale)
+    #change our hex colours string in to tuples
+    
+    tGuanine = hex2rgb(guanine)
+    tThymine = hex2rgb(thymine)
+    tAdenine = hex2rgb(adenine)
+    tCytosine = hex2rgb(cytosine)
+    
+   
+
+    pixelArray = genBitMap2(imageArray, rowLen, rows, scale, tGuanine, tThymine, tAdenine, tCytosine)
 #size the bitmap according to the number of elements in the array 
     
     
